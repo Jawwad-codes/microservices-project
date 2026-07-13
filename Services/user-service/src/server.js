e045e61a647f1fea9473696378fe191e4cdffb3b
Jawwad-codes <jawwadnadeem78@gmail.com>
2026-07-13 12:29:23 +0500

    fix docker env issues

--- Services/user-service/src/server.js
+++ Services/user-service/src/server.js
@@ -2,3 +2,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
+
+console.log("DATABASE_URL:", JSON.stringify(process.env.DATABASE_URL));
+console.log("PORT:", process.env.PORT);
